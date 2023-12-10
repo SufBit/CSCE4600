94e807208351ca8c93207cdaa8a1234dd60c0a12
SufBit <sufarshad.99@gmail.com>
2023-12-10 14:59:29 -0600

    everyhting working

--- Project2/builtins/pwd_test.go
+++ Project2/builtins/pwd_test.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"testing"
 	"os"
+	"fmt"
 	"github.com/stretchr/testify/require"
 )
 
@@ -12,14 +13,16 @@ func TestPrintWorkingDirectory(t *testing.T) {
 	t.Parallel()
 
 	t.Run("success: print working directory", func(t *testing.T) {
-		// Run PrintWorkingDirectory
-		var out bytes.Buffer
-		err := PrintWorkingDirectory(&out)
-		require.NoError(t, err, "unexpected error")
+        // Run PrintWorkingDirectory
+        var out bytes.Buffer
+        err := PrintWorkingDirectory(&out)
+        require.NoError(t, err, "unexpected error")
 
-		// Check if the output contains the current working directory
-		wd, err := os.Getwd()
-		require.NoError(t, err, "failed to get current working directory")
-		require.Contains(t, out.String(), wd, "output does not contain working directory")
-	})
+        // Print the current working directory for debugging
+        wd, _ := os.Getwd()
+        fmt.Println("Current working directory:", wd)
+
+        // Check if the output contains the current working directory
+        require.Contains(t, out.String(), wd, "output does not contain working directory")
+    })
 }
