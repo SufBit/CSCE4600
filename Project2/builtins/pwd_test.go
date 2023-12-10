05a1e6d34e9dcfb179d0f527e948c2b2b9c76f11
SufBit <sufarshad.99@gmail.com>
2023-12-10 15:23:37 -0600

    everything working

--- Project2/builtins/pwd_test.go
+++ Project2/builtins/pwd_test.go
@@ -1,39 +1,3 @@
-// // builtins/pwd_test.go
-// package builtins
-
-// import (
-// 	"bytes"
-// 	"testing"
-// 	"os"
-	
-// )
-
-// func TestPrintWorkingDirectory(t *testing.T) {
-// 	t.Parallel()
-
-// 	// Run PrintWorkingDirectory
-// 	var out bytes.Buffer
-// 	err := PrintWorkingDirectory(&out)
-// 	if err != nil {
-// 		t.Fatalf("Error printing working directory: %v", err)
-// 	}
-
-// 	// Check if the output contains a non-empty working directory
-// 	actual := out.String()
-// 	if len(actual) == 0 {
-// 		t.Error("Printed working directory is empty")
-// 	}
-
-// 	// Print the working directory and printed working directory for debugging
-// 	wd, err := os.Getwd()
-// 	if err != nil {
-// 		t.Fatalf("Error getting current working directory: %v", err)
-// 	}
-
-// 	t.Logf("Current working directory: %s", wd)
-// 	t.Logf("Printed working directory: %s", actual)
-// }
-
 // builtins/pwd_test.go
 package builtins
 
@@ -47,7 +11,7 @@ func TestPrintWorkingDirectory(t *testing.T) {
 	t.Parallel()
 
 	// Create a temporary directory
-	tempDir, err := ioutil.TempDir("", "pwd_test")
+	tempDir, err := os.MkdirTemp("", "pwd_test")
 	if err != nil {
 		t.Fatalf("Error creating temporary directory: %v", err)
 	}
